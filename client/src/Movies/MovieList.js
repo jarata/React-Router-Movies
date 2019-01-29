811d7e12558bd1555743977ada5c21c078fc9320
J A <jarata@gmail.com>
2019-01-29 15:17:15 -0800

    Part 1 and most of Part 2 done

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -37,7 +37,7 @@ function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
     <div className="movie-card">
-        <Link to={`movies/:${movie.id}`}>
+        <Link to={`movies/${movie.id}`}>
             <h2>{title}</h2>
             <div className="movie-director">
                 Director: <em>{director}</em>
