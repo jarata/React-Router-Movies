f34caee37bc8100ff0c120b85238966e5bc33405
J A <jarata@gmail.com>
2019-01-29 13:47:18 -0800

    trying to refine Link

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ export default class App extends Component {
         <SavedList list={this.state.savedList} />
         <div>Replace this Div with your Routes</div>
         <Route exact path='/' component={MovieList} />
-        <Route path={`/movies/:${id}`} component={Movie} />
+        <Route path={`/movies/:id`} component={Movie} />
       </div>
     );
   }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
+import Movie from './Movie'
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -34,21 +36,23 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <Link to={`/movies/:${id}`} className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+    <div className="movie-card">
+        <Link to={`movies/:${movie.id}`}>
+            <h2>{title}</h2>
+            <div className="movie-director">
+                Director: <em>{director}</em>
+            </div>
+            <div className="movie-metascore">
+                Metascore: <strong>{metascore}</strong>
+            </div>
+            <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </Link>
+            {stars.map(star => (
+                <div key={star} className="movie-star">
+                {star}
+                </div>
+            ))}
+        </Link>
+    </div>
   );
 }
