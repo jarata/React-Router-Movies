7984a38d9c6d57581c21935b7923f761792fd52d
Justin A <jarata@gmail.com>
2019-12-18 15:34:28 -0800

    ateempting to wire the saved list feature

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList( [...savedList, movie] );
@@ -16,7 +16,7 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList}/>
-      <Route path="/movies/:id" component={Movie}/>
+      <Route path="/movies/:id" component={Movie} addToSavedList={addToSavedList}/>
     </div>
   );
 };
